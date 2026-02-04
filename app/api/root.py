7cb3867df679bbd95d3d917e902d54faa1b8bdfd
alex6712 <alexeivanyukov@yandex.ru>
2026-02-04 14:38:32 +0300

    /health вынесен в корень API

--- app/api/root.py
+++ app/api/root.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter, status
 from fastapi.responses import PlainTextResponse
 
 from app.core.dependencies.settings import SettingsDependency
+from app.schemas.v1.responses.app_info import AppInfoResponse
+from app.schemas.v1.responses.standard import StandardResponse
 
 api_root_router = APIRouter(
-    include_in_schema=False,
+    tags=["root"],
 )
 
 
@@ -14,6 +16,7 @@ api_root_router = APIRouter(
     status_code=status.HTTP_200_OK,
     summary="Получение файла robots.txt для ботов.",
     response_description="Файл robots.txt для API",
+    include_in_schema=False,
 )
 async def robots_txt(settings: SettingsDependency) -> str:
     """Получение файла robots.txt для ботов.
@@ -32,3 +35,61 @@ async def robots_txt(settings: SettingsDependency) -> str:
         Текст файла robots.txt.
     """
     return settings.ROBOTS_CONTENT
+
+
+@api_root_router.get(
+    "/health",
+    response_model=StandardResponse,
+    status_code=status.HTTP_200_OK,
+    summary="Проверка работоспособности API.",
+    response_description="API работает",
+)
+async def health() -> StandardResponse:
+    """Путь для проверки работоспособности API.
+
+    Ничего не делает, кроме как возвращает положительный ответ на запрос.
+
+    Returns
+    -------
+    StandardResponse
+        Ответ о корректной работе сервера.
+    """
+    return StandardResponse(detail="API works!")
+
+
+@api_root_router.get(
+    "/app_info",
+    response_model=AppInfoResponse,
+    status_code=status.HTTP_200_OK,
+    summary="Получение информации о приложении.",
+    response_description="Информация о приложении",
+)
+async def app_info(settings: SettingsDependency) -> AppInfoResponse:
+    """Запрос на получение информации о серверной стороне приложения.
+
+    Получаемая информация:
+    - app_name: str, имя приложения;
+    - app_version: str, версия приложения;
+    - app_description: str, полное описание приложения;
+    - app_summary: str, краткое описание приложения;
+    - admin_name: str, полное имя ответственного лица;
+    - admin_email: str, адрес электронной почты для связи с ответственным лицом.
+
+    Parameters
+    ----------
+    settings : Settings
+        Настройки приложения.
+
+    Returns
+    -------
+    AppInfoResponse
+        Ответ, содержащий информацию о серверной стороне приложения.
+    """
+    return AppInfoResponse(
+        app_name=settings.APP_NAME,
+        app_version=settings.APP_VERSION,
+        app_description=settings.APP_DESCRIPTION,
+        app_summary=settings.APP_SUMMARY,
+        admin_name=settings.ADMIN_NAME,
+        admin_email=settings.ADMIN_EMAIL,
+    )
--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,7 +4,6 @@ from app.api.v1.auth import router as _auth_router
 from app.api.v1.couples import router as _couples_router
 from app.api.v1.media import router as _media_router
 from app.api.v1.notes import router as _notes_router
-from app.api.v1.root import router as _root_router
 from app.api.v1.users import router as _users_router
 
 api_v1_router = APIRouter(
@@ -15,5 +14,4 @@ api_v1_router.include_router(_auth_router)
 api_v1_router.include_router(_couples_router)
 api_v1_router.include_router(_media_router)
 api_v1_router.include_router(_notes_router)
-api_v1_router.include_router(_root_router)
 api_v1_router.include_router(_users_router)
