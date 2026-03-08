66818267b95d3cdd6792ccc02cdbbb208fc7bbbe
alex6712 <alexeivanyukov@yandex.ru>
2026-03-08 11:51:08 +0300

    Небольшое уточнение контрактов API

--- app/schemas/v1/requests/files.py
+++ app/schemas/v1/requests/files.py
@@ -45,10 +45,49 @@ class UploadFilesBatchRequest(BaseModel):
     ----------
     files_metadata : list[UploadFileRequest]
         Метаданные для каждого файла.
+        Ограничения: минимум один элемент, максимум 50 элементов.
     """
 
     files_metadata: list[UploadFileRequest] = Field(
         description="Метаданные для каждого файла.",
+        min_length=1,
+        max_length=50,
+    )
+
+
+class ConfirmUploadRequest(BaseModel):
+    """Схема запроса на подтверждения завершения загрузки медиа-файла.
+
+    Attributes
+    ----------
+    file_id : UUID
+        UUID загруженного файла.
+    """
+
+    file_id: UUID = Field(
+        description="UUID загруженного файла.",
+        examples=["ccdc1e34-8772-4537-bdba-5e45c4be5d7c"],
+    )
+
+
+class DownloadFilesBatchRequest(BaseModel):
+    """Схема запроса на получение Presigned URLs скачивания пакета файлов.
+
+    Attributes
+    ----------
+    files_uuids : list[UUID]
+        Список UUID медиа-файлов для скачивания.
+        Ограничения: минимум один UUID, максимум 50 UUID.
+    """
+
+    files_uuids: list[UUID] = Field(
+        description="Список UUID медиа-файлов, которые необходимо скачать на клиент.",
+        examples=[
+            [
+                "681cbf12-fe3f-41f4-92f1-c8cb33dfe47e",
+                "f466bb69-bf31-4125-a29a-35166033e4ef",
+            ]
+        ],
         min_length=1,
         max_length=50,
     )
@@ -81,39 +120,4 @@ class PatchFileRequest(BaseModel):
         default_factory=lambda: UNSET,
         description="Описание медиа файла",
         examples=["Файл смерти: кто прочитал, тот..."],
-    )
-
-
-class ConfirmUploadRequest(BaseModel):
-    """Схема запроса на подтверждения завершения загрузки медиа-файла.
-
-    Attributes
-    ----------
-    file_id : UUID
-        UUID загруженного файла.
-    """
-
-    file_id: UUID = Field(
-        description="UUID загруженного файла.",
-        examples=["ccdc1e34-8772-4537-bdba-5e45c4be5d7c"],
-    )
-
-
-class DownloadFilesBatchRequest(BaseModel):
-    """Схема запроса на получение Presigned URLs скачивания пакета файлов.
-
-    Attributes
-    ----------
-    files_uuids : list[UUID]
-        Список UUID медиа-файлов для скачивания.
-    """
-
-    files_uuids: list[UUID] = Field(
-        description="Список UUID медиа-файлов, которые необходимо скачать на клиент.",
-        examples=[
-            [
-                "681cbf12-fe3f-41f4-92f1-c8cb33dfe47e",
-                "f466bb69-bf31-4125-a29a-35166033e4ef",
-            ]
-        ],
     )
