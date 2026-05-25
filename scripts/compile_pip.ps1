e039b41bfb9897dc97f442d6dc244145f7c23797
alex6712 <alexeivanyukov@yandex.ru>
2026-05-25 08:26:21 +0300

    Актуализация зависимостей приложения; обновлён скрипт создания requirements.txt на Windows

--- scripts/compile_pip.ps1
+++ scripts/compile_pip.ps1
@@ -10,10 +10,10 @@ if (-not (Test-Path "pyproject.toml")) {
 
 # Генерация requirements-dev.txt
 Write-Host "requirements-dev.txt generation..."
-uv pip compile pyproject.toml --group dev -o requirements-dev.txt
+uv pip compile pyproject.toml -U --group dev -o requirements-dev.txt
 
 # Генерация requirements.txt
 Write-Host "requirements.txt generation..."
-uv pip compile pyproject.toml -o requirements.txt
+uv pip compile pyproject.toml -U -o requirements.txt
 
 Write-Host "requirements files generated!"
